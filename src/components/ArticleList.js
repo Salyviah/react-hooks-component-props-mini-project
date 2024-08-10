78d285fbf060e1db993fd292e33a9dcb67a7bc06
salyviah <salyviahkamau@gmail.com>
2024-08-10 15:27:55 +0300

    react-hooks-component-props-mini-project

---
+++ src/components/ArticleList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Article from './Article'
+
+function ArticleLIst({posts}){
+    return( 
+    <main>
+        {posts.map((post) =>(
+        <Article key={post.id} title = {post.title} date = {post.date} preview = {post.preview} />
+
+        ))}
+
+    </main>
+    );
+}
+
+export default ArticleLIst;
